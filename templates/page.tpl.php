e6c5d3e90c3af627900d31ae7bfbd428dc643136
Lucas Weeks <ldweeks@gmail.com>
2012-09-19 17:44:05 +0000

    Ben: modified info file and modified page.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,7 +73,7 @@
 <div id="page">
 
   <header id="header" role="banner">
-
+   <div class="headerContainer">
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
@@ -111,31 +111,9 @@
     <a href="http://dev3.clearnotesongbook.com/media-player/clearnote-songbook-player" title="<?php print t('Listen Now!'); ?>" target="_blank">Play Audio.</a>
 
     <?php print render($page['header']); ?>
-
-  </header>
-
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div><!-- /#content -->
-
-    <div id="navigation">
+   </div>
+   <div class="mainNav">
+     <div id="navigation">
 
       <?php if ($main_menu): ?>
         <nav id="main-menu" role="navigation">
@@ -161,6 +139,34 @@
       <?php print render($page['navigation']); ?>
 
     </div><!-- /#navigation -->
+   </div>
+  </header>
+
+  <div id="main">
+	<div class="breadcrumb">
+      <?php print $breadcrumb; ?>
+    </div>
+    <div class="aboveMain">
+      <?php print render($page['highlighted']); ?>
+      
+    </div>
+    <div id="content" class="column" role="main">
+      
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div><!-- /#content -->
 
     <?php
       // Render the sidebars to see if there's anything in them.
