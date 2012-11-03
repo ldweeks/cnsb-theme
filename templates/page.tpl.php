2ecdf3c9d7b14bd92d22c4e87b786f3fdb423c0d
Lucas Weeks <ldweeks@gmail.com>
2012-11-03 12:39:00 -0500

    Added mailchimp subscription block.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -167,4 +167,20 @@
 <span class='st_pinterest_hcount' displayText='Pinterest'></span>
 <span class='st_email_hcount' displayText='Email'></span>
 <span class='st_sharethis_hcount' displayText='ShareThis'></span>
+<!-- Begin MailChimp Signup Form -->
+<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="http://clearnotebloomington.us2.list-manage2.com/subscribe/post?u=844249c230081fdb270c11d7d&amp;id=b2cd815d83" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<label for="mce-EMAIL">Stay in touch!</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 <?php print render($page['bottom']); ?>
