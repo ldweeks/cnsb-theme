af4d77f30a04b7dd616114eaf32c93ec32ce6372
Lucas Weeks <ldweeks@gmail.com>
2012-11-02 22:33:16 -0500

    Added social media sharing buttons to the footer

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -160,4 +160,10 @@
 
 </div><!-- /#page -->
 <?php print render($page['footer']); ?>
+<span class='st_facebook_hcount' displayText='Facebook'></span>
+<span class='st_fblike_hcount' displayText='Facebook Like'></span>
+<span class='st_twitter_hcount' displayText='Tweet'></span>
+<span class='st_pinterest_hcount' displayText='Pinterest'></span>
+<span class='st_email_hcount' displayText='Email'></span>
+<span class='st_sharethis_hcount' displayText='ShareThis'></span>
 <?php print render($page['bottom']); ?>
